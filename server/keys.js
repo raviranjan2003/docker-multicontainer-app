eebd9fcd36b9b6af6d973a7d2f969824f5c2680d
raviranjan2003 <2140131@sliet.ac.in>
2024-03-12 17:55:17 +0530

    server created for postgre

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    redisHost : process.env.REDIS_PORT,
+    redisPort : process.env.REDIS_PORT,
+    pgUser : process.env.PGUSER,
+    pgHost : process.env.PGHOST,
+    pgDatabase : process.env.PRDATABASE,
+    pgPassword : process.env.PGPASSWORD,
+    pgPort : process.env.PGPORT
+}
